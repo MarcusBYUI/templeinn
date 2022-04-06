fd37fd77397e78275267ad74d5d65823106b864e
MarcusBYUI <ojo18002@byui.edu>
2022-04-06 21:58:58 +0100

    fixed images

--- js/temples.js
+++ js/temples.js
@@ -37,7 +37,9 @@ function makeCard(tempList) {
     //the template
     return `
         <div class="temple-card">
-            <div class="temple-image">
+            <div class="temple-image" style="background-image: url(../images/${
+              temple.image
+            })">
               <div class="temple-name-overlay">
                 <p>${temple.name}</p>
                 <a href="${temple.link}" target="_blank">
